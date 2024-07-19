a2ad09c3d1c76fe945d3d2e6e6bd9f20c07ef94e
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2024-07-18 20:05:16 -0700

    Add: select options between standard build test or running a special test where a flake update is first performed for specific use cases

--- build.sh
+++ build.sh
@@ -3,82 +3,110 @@ green='\033[1;32m'
 cyan='\033[1;36m'
 red='\033[1;31m'
 noColor='\033[0m'
-test=true
 
 fCommit() {
     echo -e "\n${green}Please write commit message: ${noColor}"
     read commitmsg
     git commit -a -m "${commitmsg}"
 }
-
 echo -e "${cyan}Welcome to the NixBuild script.${noColor}"
 sleep 1
 
 cd ~/nixconf
 
-until [ "${test}" = false ]; do
+select testType in "Build Test" "Flake Update Test"
+do
+    case $testType in
+        "Build Test")
+            until [ "${test}" = "n" ]; do
+                    echo -e "${green}Adding all to working directory.${noColor}"
+                    git add . 
 
-    echo -e "${cyan}Would you like to test? [${red}Y${cyan}/${green}n${cyan}]${noColor}"
-    read -n 1 test
+                    echo -e "${green}Formatting Nix code.${noColor}"
+                    alejandra .
 
-    if [ "${test}" = "n" ]; then
-        test=false
-    else
-        echo -e "${green}Adding all to working directory.${noColor}"
-        git add . 
+                    rm ${HOME}/.gtkrc-2.0 #removing the backup of the annoying file :(
+                    
+                    echo -e "${green}Running build test.${noColor}"
+                    nh os test
 
-        echo -e "${green}Formatting Nix code.${noColor}"
-        alejandra .
+                    echo -e "${cyan}Would you like to commit the changes? [${red}Y${cyan}/${green}n${cyan}]${noColor}"
+                    read -n 1 commit
+                    if [ "${commit}" != "n" ]; then
+                        fCommit
+                    fi
 
-        rm ${HOME}/.gtkrc-2.0 #removing the backup of the annoying file :(
+                echo -e "${cyan}Would you like to test again? [${red}Y${cyan}/${green}n${cyan}]${noColor}"
+                read -n 1 test
+            done
+        break;;
+        "Flake Update Test")
+            until [ "${test}" = "n" ]; do
+                rm ${HOME}/.gtkrc-2.0 #removing the backup of the annoying file :(
 
-        nh os test
+                echo -e "${green}Running flake update test.${noColor}"
+                nh os test --update
 
-        echo -e "${cyan}Would you like to commit the changes? [${red}Y${cyan}/${green}n${cyan}]${noColor}"
-        read -n 1 commit
-        if [ "${commit}" != "n" ]; then
-            fCommit
-        fi
+                echo -e "${green}Adding all to working directory.${noColor}"
+                git add . 
+                
+                echo -e "${green}Formatting Nix code.${noColor}"
+                alejandra .
+                
+                echo -e "${green}Running build test.${noColor}"
+                nh os test
 
-    fi
+                echo -e "${cyan}Would you like to commit the changes? [${red}Y${cyan}/${green}n${cyan}]${noColor}"
+                read -n 1 commit
+                if [ "${commit}" != "n" ]; then
+                    fCommit
+                fi
+
+                echo -e "${cyan}Would you like to test again? [${red}Y${cyan}/${green}n${cyan}]${noColor}"
+                read -n 1 test
+            done
+        break;;
+        *)
+            echo "Invalid Option";;
+    esac
 done
 
-if [ "${commit}" = "n" ]; then
-    fCommit
-fi
-
-echo -e "${cyan}Switch now or at boot? [${red}switch${cyan}/${green}boot${cyan}]${noColor}"
-read switch
-
-echo -e "${cyan}Update? [${red}y${cyan}/${green}N${cyan}]${noColor}"
-read -n 1 update
-
-rm ${HOME}/.gtkrc-2.0
-
-if [ "${update}" = "y" ]; then
-    nh os ${switch} --update --ask #asks so you dont get shocked by everything restarting
-
-    echo -e "${green}Committing update.${noColor}"
-    git commit -a -m "Flake Update"
-else
-    nh os ${switch}
-fi
-
-echo -e "${cyan}Would you like to push to remote? [${red}Y${cyan}/${green}n${cyan}]${noColor}"
-read -n 1 push
-
-if [ "${push}" != "n" ]; then
-    echo -e "${cyan}Rebase before pushing? [${red}y${cyan}/${green}N${cyan}]${noColor}"
-    read -n 1 rebase
-
-    if [ "${rebase}" = "y" ]; then
-        git rebase --interactive
+    if [ "${commit}" = "n" ]; then
+        fCommit
     fi
-    
-    git push
-fi
 
-echo -e "${green}Cleaning NixOS.${noColor}"
-nh clean all --keep 5 --ask #asking so i can verify it doesn't do smthn i dont want
+    echo -e "${cyan}Switch now or at boot? [${red}switch${cyan}/${green}boot${cyan}]${noColor}"
+    read switch
 
-exit
+    echo -e "${cyan}Update? [${red}y${cyan}/${green}N${cyan}]${noColor}"
+    read -n 1 update
+
+    rm ${HOME}/.gtkrc-2.0
+
+    if [ "${update}" = "y" ]; then
+        nh os ${switch} --update --ask #asks so you dont get shocked by everything restarting
+
+        echo -e "${green}Committing update.${noColor}"
+        git commit -a -m "Flake Update"
+    else
+        nh os ${switch}
+    fi
+
+    echo -e "${cyan}Would you like to push to remote? [${red}Y${cyan}/${green}n${cyan}]${noColor}"
+    read -n 1 push
+
+    if [ "${push}" != "n" ]; then
+        echo -e "${cyan}Rebase before pushing? [${red}y${cyan}/${green}N${cyan}]${noColor}"
+        read -n 1 rebase
+
+        if [ "${rebase}" = "y" ]; then
+            git rebase --interactive
+        fi
+
+        git push
+    fi
+
+    echo -e "${green}Cleaning NixOS.${noColor}"
+    nh clean all --keep 5 --ask #asking so i can verify it doesn't do smthn i dont want
+
+    exit
