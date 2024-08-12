ea72aee157ed4ef740f65394b5fd10a1115ab80a
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2024-08-11 21:37:40 -0700

    Feat: Delete Branch

--- scripts/branch.bash
+++ scripts/branch.bash
@@ -1,7 +1,7 @@
 echo -e "${red}Current branches on repo:"
 git show-branch --list
 
-select branchOption in "Switch Branch" "Create New" "Return"
+select branchOption in "Switch Branch" "Create New" "Delete Branch" "Return"
 do
     case $branchOption in
     "Switch Branch")
@@ -11,10 +11,23 @@ do
     ;;
     "Create New")
         echo -e "${green}Name the new branch:${noColor}"
-        read branchName
-        if (git switch -c ${branchName}) then
-            git commit -a -m "Init New Branch: ${branchName}"
+        read newBranch
+        if (git switch -c ${newBranch}) then
+            git commit -a -m "Init New Branch: ${newBranch}"
             git push -u
+        fi
+    ;;
+    "Delete Branch")
+        echo -e "${green}Name of the branch to delete:${noColor}"
+        read delBranch
+
+        echo -e "${cyan}Warning: This action cannot be undone. Are you sure you would like to delete branch ${red}'${delbranch}'${cyan}? [${red}y${cyan}/${green}N${cyan}]${noColor}"
+        read -n 1 delete
+        if [ "$delete" = "y" ]; then
+            git push -d origin ${delBranch}
+            git branch -d ${delBranch}
+        else
+            echo -e "${green}Branch deletion cancelled.${noColor}"
         fi
     ;;
     "Return")
