02d338d879847ef37e97d13da469ee0a4fb0ac4a
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2024-09-04 18:14:29 -0700

    Add: pkg tree fetching

--- scripts/fetch.bash
+++ scripts/fetch.bash
@@ -1,10 +1,19 @@
-select branchOption in "Fetch URL" "Return"
+select branchOption in "Fetch URL" "Fetch pkg Tree" "Return"
 do
     case $branchOption in
     "Fetch URL")
         echo -e "${green}URL:${noColor}"
         read url
         nix-prefetch-url "${url}"
+    ;;
+    "Fetch pkg Tree")
+        echo -e "${green}pkg:${noColor}"
+        read pkg
+
+        if (nix build nixpkgs#${pkg}); then
+            cd result
+            nix run nixpkgs#eza -- --tree
+        fi
     ;;
     "Return")
         break;;
