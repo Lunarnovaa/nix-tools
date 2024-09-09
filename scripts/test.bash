033f84d376783e60380853337f6d893daa4d1ce5
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2024-09-08 18:39:16 -0700

    Feat: boot+update test

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,3 +1,13 @@
+commitRequest () {
+    echo -e "${cyan}Would you like to commit the changes? [${red}y${cyan}/${green}N${cyan}]${noColor}"
+    read -n 1 commit
+
+    if [ "${commit}" = "y" ]; then
+        echo -e "\n${green}Please write commit message: ${noColor}"
+        read commitmsg
+        git commit -a -m "${commitmsg}"
+    fi
+}
 
 echo -e "${cyan}"
 select testOption in "Basic Test" "Flake Update Test" "Boot Test" "Return"
@@ -14,44 +24,30 @@ do
     "Basic Test")
         echo -e "${green}Running build test.${noColor}"
         if (nh os test) then
-
-            echo -e "${cyan}Would you like to commit the changes? [${red}y${cyan}/${green}N${cyan}]${noColor}"
-            read -n 1 commit
-
-            if [ "${commit}" = "y" ]; then
-                echo -e "\n${green}Please write commit message: ${noColor}"
-                read commitmsg
-                git commit -a -m "${commitmsg}"
-            fi
+            commitRequest
         fi
     ;;
     "Flake Update Test")
     
         echo -e "${green}Rebuilding with flake update.${noColor}"
         if (nh os test --update) then
-
-            echo -e "${cyan}Would you like to commit the changes? [${red}y${cyan}/${green}N${cyan}]${noColor}"
-            read -n 1 commit
-
-            if [ "${commit}" = "y" ]; then
-                echo -e "\n${green}Please write commit message: ${noColor}"
-                read commitmsg
-                git commit -a -m "${commitmsg} + Flake Update"
-            fi
+            commitRequest
         fi
 
     ;;
     "Boot Test")
-        echo -e "${green}Running boot test.${noColor}"
-        if (nh os boot) then
+        echo -e "${cyan}Would you like to update the flake as well? [${red}y${cyan}/${green}N${cyan}]${noColor}"
+        read -n 1 testUpdate
 
-            echo -e "${cyan}Would you like to commit the changes? [${red}y${cyan}/${green}N${cyan}]${noColor}"
-            read -n 1 commit
-
-            if [ "${commit}" = "y" ]; then
-                echo -e "\n${green}Please write commit message: ${noColor}"
-                read commitmsg
-                git commit -a -m "${commitmsg}"
+        if [ "${testUpdate}" = "y" ]; then
+            echo -e "${green}Running boot test plus flake update.${noColor}"
+            if (nh os boot --update) then
+                commitRequest
+            fi
+        else
+            echo -e "${green}Running boot test.${noColor}"
+            if (nh os boot) then
+                commitRequest
             fi
         fi
     ;;
